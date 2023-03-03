c01afbbe288d21117a8acbed5e7b7f526d21ed18
benchi-yin <126649953+benchi-yin@users.noreply.github.com>
2023-03-03 16:02:59 +0800

    3.3-16.3

--- js/script.js
+++ js/script.js
@@ -3,7 +3,6 @@ $(document).ready(function() {
     $('.navigation').append('<area shape="poly" coords="224,82, 339,149, 339,279, 224,346, 109,279, 109,149" href="#close" alt="Jammy�s Folio ui designer &amp; developer" class="logo">')
     $('.navigation').append('<area shape="poly" coords="147,-5, 219,40, 219,120, 147,160, 74,120, 74,40" href="#!/page_Home" alt="" class="nav1">')
     $('.navigation').append('<area shape="poly" coords="73,129, 145,174, 145,254, 73,294, 0,254, 0,174" href="#!/page_About" alt="" class="nav2">')
-   // $('.navigation').append('<area shape="poly" coords="10,10, 200,200, 145,254, 73,294, 0,10, 0,10" href="#!/page_About" alt="" class="nav2">')
     $('.navigation').append('<area shape="poly" coords="147,261, 219,307, 219,387, 147,427, 74,387, 74,307" href="#!/page_Talk" alt="" class="nav3">')
     $('.navigation').append('<area shape="poly" coords="300,261, 372,307, 372,387, 300,427, 228,387, 228,307" href="#!/page_Message" alt="" class="nav4">')
     $('.navigation').append('<area shape="poly" coords="379,129, 451,174, 451,254, 379,294, 306,254, 306,174" href="#!/page_Blog" alt="" class="nav5">')
@@ -11,17 +10,16 @@ $(document).ready(function() {
 
     $('.map_back').append('<area shape="poly" coords="73,-5, 145,40, 145,120, 73,160, 0,120, 0,40" href="#close" alt="" id="back">')
 
-    $('.map_logo').append('<area shape="poly" coords="115,-7, 230,45, 200,45, 115,257, 0,190, 0,60" href="#close" alt="Jammy�s Folio ui designer &amp; developer" class="logo">')
+    $('.map_logo').append('<area shape="poly" coords="115,-7, 230,60, 230,190, 115,257, 0,190, 0,60" href="#close" alt="Jammy�s Folio ui designer &amp; developer" class="logo">')
 
     // hover
     var n;
 
-    $('#logo img').stop().animate({width:160, height:160, left:66, top:66}, 600, 'easeOutBack')
     $('.navigation area').hover(function(){
         n=$(this).attr('class');
         fl2=false;
         if (n=='logo') {
-           $('#logo img').stop().animate({width:200, height:200, left:45, top:45}, 600, 'easeOutBack')
+            $('#logo img').stop().animate({width:270, height:294, left:5, top:5}, 600, 'easeOutBack')
         } else {
             n=parseInt(n.slice(-1))
             $('#nav'+n+'> img').css({zIndex:8}).stop().animate({width:165, height:177, left:5, top:5}, 600, 'easeOutBack', function(){$(this).css({zIndex:7})})
@@ -30,7 +28,7 @@ $(document).ready(function() {
     }, function(){
         var n=$(this).attr('class');
         if (n=='logo') {
-            $('#logo img').stop().animate({width:160, height:160, left:66, top:66}, 600, 'easeOutBack')
+            $('#logo img').stop().animate({width:230, height:254, left:25, top:25}, 600, 'easeOutBack')
         } else {
             n=parseInt(n.slice(-1));
             $('#nav'+n+'> img').stop().animate({width:145, height:157, left:15, top:15}, 600, 'easeOutBack', function(){$(this).css({zIndex:6})})
@@ -39,9 +37,9 @@ $(document).ready(function() {
     })
 
     $('.logo').hover(function(){
-        $('#logo img').stop().animate({width:200, height:200, left:45, top:-55}, 600, 'easeOutBack')
+        $('#logo img').stop().animate({width:270, height:294, left:5, top:5}, 600, 'easeOutBack')
     }, function(){
-        $('#logo img').stop().animate({width:160, height:160, left:66, top:66}, 600, 'easeOutBack')
+        $('#logo img').stop().animate({width:230, height:254, left:25, top:25}, 600, 'easeOutBack')
     })
 
     $('#back, .back a').hover(function(){
@@ -274,24 +272,24 @@ $(window).load(function() {
     })
 
     function googleMap(){
-        
-     if(googleMap.ready)
-     return false
-     googleMap.ready=true
-     ;(function afterAnimation(){
-     var tmp=$('*:animated')
-     if(tmp.length)
-     $.when(tmp)
-     .then(afterAnimation)
-     else{
-     var cssPath='.google_map'
-     ,holder=$(cssPath)
-     ,src='http://maps.google.com/maps?f=q&amp;source=s_q&amp;hl=en&amp;geocode=&amp;q=Brooklyn,+New+York,+NY,+United+States&amp;aq=0&amp;sll=37.0625,-95.677068&amp;sspn=61.282355,146.513672&amp;ie=UTF8&amp;hq=&amp;hnear=Brooklyn,+Kings,+New+York&amp;ll=40.649974,-73.950005&amp;spn=0.01628,0.025663&amp;z=14&amp;iwloc=A&amp;output=embed'
-     ,str='<iframe class="blo" width="'+holder.width()+'" height="'+holder.height()+'" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="'+src+'"></iframe>'
-     holder.html(str)
-     }
-     })()
-         
+        /*
+         if(googleMap.ready)
+         return false
+         googleMap.ready=true
+         ;(function afterAnimation(){
+         var tmp=$('*:animated')
+         if(tmp.length)
+         $.when(tmp)
+         .then(afterAnimation)
+         else{
+         var cssPath='.google_map'
+         ,holder=$(cssPath)
+         ,src='http://maps.google.com/maps?f=q&amp;source=s_q&amp;hl=en&amp;geocode=&amp;q=Brooklyn,+New+York,+NY,+United+States&amp;aq=0&amp;sll=37.0625,-95.677068&amp;sspn=61.282355,146.513672&amp;ie=UTF8&amp;hq=&amp;hnear=Brooklyn,+Kings,+New+York&amp;ll=40.649974,-73.950005&amp;spn=0.01628,0.025663&amp;z=14&amp;iwloc=A&amp;output=embed'
+         ,str='<iframe class="blo" width="'+holder.width()+'" height="'+holder.height()+'" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="'+src+'"></iframe>'
+         holder.html(str)
+         }
+         })()
+         */
     }
 
     var m_top=0, h_cont=730;
